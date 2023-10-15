6007132c33e2f24417953f8b5f579c9f30b5bd2e
Dmytro Sayko <saykodmytro@icloud.com>
2023-10-15 08:43:15 +0300

    up Profile Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
+import { Profile } from './Profile/Profile';
+import user from '../jsonData/user.json';
+import css from '../components/App.module.css';
+
 export const App = () => {
-  return <div>React homework test</div>;
+  return (
+    <div className={css.container}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
 };
